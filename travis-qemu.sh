9b65eb811df02279a8a65cc2fbca495de675a403
kevin <kk@sudo-i.net>
2021-03-28 17:48:20 +0200

    bal

--- travis-qemu.sh
+++ travis-qemu.sh
@@ -37,8 +37,3 @@ make -j4
 make install
 
 echo "$VERSION $TARGETS" > $HOME/qemu/.build
-sudo apt remove packer
-curl -fsSL https://apt.releases.hashicorp.com/gpg | sudo apt-key add -
-sudo apt-add-repository "deb [arch=amd64] https://apt.releases.hashicorp.com $(lsb_release -cs) main"
-sudo apt-get update && sudo apt-get install packer -y
-packer -v
